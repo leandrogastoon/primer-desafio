0af1f9c518ac8362e86be025fe730a305cef142d
leandro martinez <llggmm23@gmail.com>
2022-08-12 11:30:42 -0300

    ciclo while y do while
    primera prueba

---
+++ desafio-1.js
@@ -0,0 +1,12 @@
+let numero = 0;
+
+while (numero <= 20){
+    numero ++;
+    document.write(numero)
+}
+let numero2 = 10;
+
+do{
+  numero2 * 10;
+  document.write(numero2)
+} while(numero2 <= 1000);
