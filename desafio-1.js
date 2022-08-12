74e049e353cf1d52c13ee963f35ed3e02e23f7db
leandro martinez <llggmm23@gmail.com>
2022-08-12 11:40:17 -0300

    segunda prueba
    sin descripcion

--- desafio-1.js
+++ desafio-1.js
@@ -1,12 +1,16 @@
 let numero = 0;
 
 while (numero <= 20){
-    numero ++;
-    document.write(numero)
+    numero++;
+    console.log(numero)
 }
-let numero2 = 10;
+
+console.log("termino el primer ciclo");
+
+
+let numero2 = 1;
 
 do{
-  numero2 * 10;
-  document.write(numero2)
-} while(numero2 <= 1000);
+  numero2 ++;
+  console.log(numero2)
+} while(numero2 < 6);
