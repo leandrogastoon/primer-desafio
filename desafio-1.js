0f37a94bccedfeb93863c380f36dd7b47b8507c5
leandro martinez <llggmm23@gmail.com>
2022-08-12 11:54:23 -0300

    ultima prueba
    entrega del desafio 1

--- desafio-1.js
+++ desafio-1.js
@@ -14,3 +14,23 @@ do{
   numero2 ++;
   console.log(numero2)
 } while(numero2 < 6);
+
+console.log("termino ciclo do while")
+
+let numero3 = 1;
+
+while(numero3 < 1000){
+    numero3++;
+    document.write(numero3);
+    if(numero3 == 10){
+        break;
+    }
+}
+
+document.write(" se corta el ciclo por el break   //   ")
+
+for (let numero4 = 0; numero4 < 6; numero4++){
+    document.write(numero4)
+}
+
+document.write(" termina el ciclo.");
