3f06b1f0699a63349360b5dad6962d1174ba64d1
leandro martinez <llggmm23@gmail.com>
2022-08-17 10:19:57 -0300

    desafio 1
    condicional y ciclo for

--- desafio-1.js
+++ desafio-1.js
@@ -3,7 +3,6 @@ let nombre = prompt("Introducir nombre del Alumno")
 if ( nombre == "Leandro") {
     for (let i = 0; i <= 10; i++) {
         console.log(i);
-        
     } console.log("El alumno ingresado es el correcto")
 } else {
     console.log("El alumno ingresado no esta registrado")
